dab75d9f455183dbc0f768dc5c57056140707df0
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-09-17 21:30:44 -0500

    Container with most water - 11

---
+++ Two Pointers/11-Container-With-Most-Water.py
@@ -0,0 +1,28 @@
+#With this problem I had to get clever with how I thought about it
+# Instead of trying to think of how to maximize an array,
+# I thought about how actual water would flow in a container,
+# Which led me to this solution based on finding the containers walls
+
+# worst case of O(N) beats 99.5% of python submissions
+
+def maxArea(self, height: List[int]) -> int:
+        largestArea = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            leftVal = height[left]
+            rightVal = height[right]
+            
+            shortest = rightVal if rightVal < leftVal else leftVal
+            thisArea = (right - left) * shortest
+
+            if thisArea > largestArea:
+                largestArea = thisArea
+            
+            if leftVal < rightVal:
+                left += 1
+            else:
+                right -= 1
+
+        return largestArea
